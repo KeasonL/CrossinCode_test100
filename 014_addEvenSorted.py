901878e9231e324c8ee3c957e63686fe6e2c58bc
KeasonL <37066807+KeasonL@users.noreply.github.com>
2019-10-31 13:41:22 +0800

    Create 014_addEvenSorted.py

---
+++ 014_addEvenSorted.py
@@ -0,0 +1,12 @@
+## 【问题】输入整数，判断是否为偶数，是则加入列表，列表满三个元素则程序退出并按从小到大顺序输出。
+n = 0
+list = []
+while n < 3:
+    a = int(input('please input a number: '))
+    if (a % 2) == 0:
+        list.append(a)
+        n += 1
+        print('%d is an even, get it. %d number(s) in the list.' % (a,n))
+    else:
+        print('%d is an odd, pass' % a)
+print(sorted(list))
