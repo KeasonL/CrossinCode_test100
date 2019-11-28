0ed6fb9e7099fc9d94b382442058a76f407b9ae7
KeasonL <37066807+KeasonL@users.noreply.github.com>
2019-11-28 14:54:48 +0800

    Update 087_BlackHoleNumber.py

--- 087_BlackHoleNumber.py
+++ 087_BlackHoleNumber.py
@@ -37,3 +37,18 @@ findBlackHoleNum(3)
 findBlackHoleNum(4)
 findBlackHoleNum(7)
 print('end')
+
+
+# 参考答案
+from functools import reduce
+def fun(n):
+    a = [int(c) for c in str(n)]
+    a.sort()
+ 
+    s1 = reduce(lambda x, y: 10 * x + y, a[::-1])
+    s2 = reduce(lambda x, y: 10 * x + y, a)
+ 
+    return n if s1 - s2 == n else fun(s1 - s2)
+ 
+res = fun(6294)
+print('res : ', res)
