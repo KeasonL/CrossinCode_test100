cb3b6fda5b9ec2c8a41a4de6f1f4eb6d63a85e56
KeasonL <37066807+KeasonL@users.noreply.github.com>
2019-11-29 12:18:20 +0800

    Create 091_ReversePrime.py

---
+++ 091_ReversePrime.py
@@ -0,0 +1,28 @@
+## 可逆素数
+## 【问题】编写程序找出1 ~ 900之间的所有可逆素数（可逆素数是指一个素数的各位数值顺序颠倒后得到的数仍为素数，如113、311）。
+
+def isPrime(n):
+    if n == 1:
+        return False
+    if n in [2, 3]:
+        return True
+    if n % 6 != 1 and n % 6 != 5:
+        return False
+    sqrt_n = int(n ** 0.5)
+    for i in range(5, sqrt_n + 1):
+        if n % i == 0 or n % ( i + 2) == 0:
+            return False
+    return True
+
+lst1 = []
+for i in range(10,901):
+    if isPrime(i):
+        lst1.append(i)
+lst2 = []
+for k in lst1:
+    kk = int(str(k)[::-1])
+    if isPrime(kk) and kk < 900:
+        if kk not in lst2:
+            lst2.append(k)
+            print(k, kk)
+print('end')
